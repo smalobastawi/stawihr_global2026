f323ebbe9531329b4a51359ad65966c0bf236b20
smalobastawi <smaloba@stawitech.com>
2026-05-19 19:26:43 +0300

    fixing bugs

--- database/migrations/2025_07_30_000000_create_employee_deductions_table.php
+++ database/migrations/2025_07_30_000000_create_employee_deductions_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             Schema::create('employee_deductions', function (Blueprint $table) {
                 $table->id();
                 $table->unsignedBigInteger('employee_id');
-                $table->unsignedInteger('payroll_deduction_type_id');
+                $table->unsignedInteger('deduction_type_id');
                 $table->string('deduction_category')->nullable();
                 $table->decimal('percentage', 8, 2)->nullable();
                 $table->decimal('rate', 8, 2)->nullable();
@@ -30,7 +30,7 @@ return new class extends Migration
                 $table->timestamps();
 
                 $table->foreign('employee_id')->references('employee_id')->on('employee')->onDelete('cascade');
-                $table->foreign('payroll_deduction_type_id')->references('id')->on('deduction_types')->onDelete('cascade');
+                $table->foreign('deduction_type_id')->references('id')->on('deduction_types');
                 $table->foreign('created_by')->references('id')->on('user')->onDelete('set null');
             });
         }
