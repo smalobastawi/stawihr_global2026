4ad97004ddc654bb4d7740a297bb4bbfe264524c
smalobastawi <smaloba@stawitech.com>
2026-05-19 16:33:59 +0300

    making changes

--- database/migrations/2017_10_05_094341_create_SP_weekly_holiday_store_procedure.php
+++ database/migrations/2017_10_05_094341_create_SP_weekly_holiday_store_procedure.php
@@ -1,30 +1,22 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateSPWeeklyHolidayStoreProcedure extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         DB::unprepared('DROP PROCEDURE IF EXISTS SP_getWeeklyHoliday');
-        DB::unprepared('CREATE PROCEDURE SP_getWeeklyHoliday()
-        BEGIN
-	        select day_name from  weekly_holiday where status=1;
-        END');
+
+        DB::unprepared("
+            CREATE PROCEDURE SP_getWeeklyHoliday()
+            BEGIN
+                SELECT day_name FROM weekly_holiday WHERE status = 1;
+            END
+        ");
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         DB::unprepared('DROP PROCEDURE IF EXISTS SP_getWeeklyHoliday');
